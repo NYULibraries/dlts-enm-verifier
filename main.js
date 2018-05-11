5cccebe01e3a9749a2400e3f75ad80d5e41db6d4
David <da70@nyu.edu>
2018-05-11 00:47:00 -0400

    Extract function getSortedTopicNamesFromEnm

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/
         console.error( 'ERROR: ' + error )
     } );
 
+function getSortedTopicNamesFromEnm() {
+    JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/76/0000007672.html", options )
+        .then( dom => {
+            var enmTopicNames = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent );
+        } )
+        .catch( error => {
+            console.error( 'ERROR: ' + error )
+        } );
+}
+
 function getSortedTopicNamesFromScript( script ) {
     var visualizationData = JSON.parse( script.replace( /^var visualizationData = /, '' ) );
     var topicNames = visualizationData.nodes.map( ( node ) => { return node.name; } ).sort();
