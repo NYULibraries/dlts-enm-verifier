78a9b688ea088d75ade70f4cda0db6da130b7f87
David <da70@nyu.edu>
2018-05-11 01:34:35 -0400

    main.js: first draft of diff

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
+const fs = require( 'fs' );
 const jsdom = require( 'jsdom' );
 const { JSDOM } = jsdom;
+const _ = require( 'lodash' );
+const request = require( 'sync-request' );
 
 const options = {
     resources : 'usable',
     runScripts: 'dangerously',
 };
 
-JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/76/0000007672.html", options )
-    .then( dom => {
-        var enmTopicNames = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent );
-    } )
-    .catch( error => {
-        console.error( 'ERROR: ' + error )
-    } );
+// JSDOM.fromFile( './tct-7672.html', {} )
+//     .then( dom => {
+//         getSortedTopicNamesFromTct( dom );
+//     } );
 
-function getSortedTopicNamesFromEnm() {
-    JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/76/0000007672.html", options )
+var response = request( 'GET', 'https://nyuapi.infoloom.nyc/api/hit/basket/62/?format=json' ),
+    tctData = JSON.parse( response.body, '' ),
+    tctTopicName = tctData.basket.display_name,
+    tctRelatedTopicNames = tctData.relations.map( relation => {
+        return relation.basket.display_name
+    } ).sort();
+
+diffNames();
+
+function diffNames() {
+    JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/00/0000000062.html", options )
         .then( dom => {
-            var enmTopicNames = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent );
+            var enmTopicNames        = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent ),
+                enmRelatedTopicNames = enmTopicNames.filter( name => {
+                    return name !== tctTopicName;
+                } );
+                inTctButNotEnm = _.difference( tctRelatedTopicNames, enmRelatedTopicNames );
+                inEnmButNotTct = _.difference( enmRelatedTopicNames, tctRelatedTopicNames );
+
+            console.log( inTctButNotEnm );
+            console.log( inEnmButNotTct );
         } )
         .catch( error => {
             console.error( 'ERROR: ' + error )
