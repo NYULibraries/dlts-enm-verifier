cba0ba8b8c039cdca0ea0ce7fb018e75b1407b21
David <da70@nyu.edu>
2018-05-12 20:27:55 -0400

    main.js: parameterize topic id and URLs, use request() instead of JSDOM.fromUrl

--- main.js
+++ main.js
@@ -14,31 +14,26 @@ const options = {
 //         getSortedTopicNamesFromTct( dom );
 //     } );
 
-var response             = request( 'GET', 'https://nyuapi.infoloom.nyc/api/hit/basket/62/?format=json' ),
-    tctData              = JSON.parse( response.body, '' ),
+var topicId              = process.argv[ 2 ],
+    tctUrl               = `https://nyuapi.infoloom.nyc/api/hit/basket/${ topicId }/?format=json`,
+    tctData              = JSON.parse( request( 'GET', tctUrl ).body, '' ),
     tctTopicName         = tctData.basket.display_name,
     tctRelatedTopicNames = tctData.relations.map( relation => {
         return relation.basket.display_name
-    } ).sort();
+    } ).sort(),
+    enmTopicPageUrl      = getEnmTopicPageUrl( topicId ),
+    enmTopicPage         = request( 'GET', enmTopicPageUrl ).body,
+    dom                  = new JSDOM( enmTopicPage ),
+    enmTopicNames        = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent ),
+    enmRelatedTopicNames = enmTopicNames.filter( name => {
+        return name !== tctTopicName;
+    } ),
+    inTctButNotEnm       = _.difference( tctRelatedTopicNames, enmRelatedTopicNames ),
+    inEnmButNotTct       = _.difference( enmRelatedTopicNames, tctRelatedTopicNames );
 
-diffNames();
+console.log( inTctButNotEnm );
+console.log( inEnmButNotTct );
 
-function diffNames() {
-    JSDOM.fromURL( "http://dlib.nyu.edu/enm/enm-web/prototypes/topic-pages/00/00/00/00/0000000062.html", options )
-        .then( dom => {
-            var enmTopicNames        = getSortedTopicNamesFromScript( dom.window.document.querySelector( 'script' ).textContent ),
-                enmRelatedTopicNames = enmTopicNames.filter( name => {
-                    return name !== tctTopicName;
-                } ),
-                inTctButNotEnm       = _.difference( tctRelatedTopicNames, enmRelatedTopicNames ),
-                inEnmButNotTct       = _.difference( enmRelatedTopicNames, tctRelatedTopicNames );
-
-            console.log( inTctButNotEnm );
-            console.log( inEnmButNotTct );
-        } )
-        .catch( error => {
-            console.error( 'ERROR: ' + error )
-        } );
 function getEnmTopicPageUrl( id ) {
     var zeroPaddedString = id.padStart( 10, "0" );
 
