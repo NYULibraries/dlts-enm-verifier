d9ab9cd254a4b60e2a8983687b4cf91616fcd266
David <da70@nyu.edu>
2018-05-30 14:09:07 -0400

    test/test.sh: add test for --cache

--- test/test.sh
+++ test/test.sh
@@ -4,13 +4,27 @@ ROOT=$(cd "$(dirname "$0")" ; cd ..; pwd -P )
 
 cd $ROOT
 
-test1='node main.js --no-cache --use-tct-local=test/tct/ --use-enm-local=test/enm/ --  36 62 826 885 2458 3131 7672 7907 9640 11928 14034 21488 22256 26141 43816'
+topicIds='36 62 826 885 2458 3131 7672 7907 9640 11928 14034 21488 22256 26141 43816'
 
-eval $test1
-diff -r --exclude .commit-empty-directory reports/ test/reports/
-if [ $? -eq 0 ]
-then
-    echo "PASS: \`${test1}\`"
-else
-    echo >&2 "FAIL: \`${test1}\`"
-fi
+test1="node main.js --no-cache --use-tct-local=test/tct/ --use-enm-local=test/enm/ -- ${topicIds}"
+verify1='diff -r --exclude .commit-empty-directory reports/ test/reports/'
+
+test2="node main.js --cache -- ${topicIds}"
+verify2='diff -r --exclude .commit-empty-directory cache/tct/ test/verify-tct-cache/'
+
+doTest() {
+    local testCmd="$1"
+    local verifyCmd="$2"
+
+    eval $testCmd
+    eval $verifyCmd
+    if [ $? -eq 0 ]
+    then
+        echo "PASS: \`${testCmd}\`"
+    else
+        echo >&2 "FAIL: \`${testCmd}\`"
+    fi
+}
+
+doTest "${test1}" "${verify1}"
+doTest "${test2}" "${verify2}"
