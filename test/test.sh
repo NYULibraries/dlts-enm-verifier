7693aa69e08dd4505363f7a05a075eebc25acc0b
David <da70@nyu.edu>
2018-05-30 13:55:39 -0400

    test/test.sh: print name of test with result

--- test/test.sh
+++ test/test.sh
@@ -3,15 +3,14 @@
 ROOT=$(cd "$(dirname "$0")" ; cd ..; pwd -P )
 
 cd $ROOT
-node main.js \
-    --no-cache --use-tct-local=test/tct/ --use-enm-local=test/enm/ -- \
-    36 62 826 885 2458 3131 7672 7907 9640 11928 14034 21488 22256 26141 43816
 
+test1='node main.js --no-cache --use-tct-local=test/tct/ --use-enm-local=test/enm/ --  36 62 826 885 2458 3131 7672 7907 9640 11928 14034 21488 22256 26141 43816'
+
+eval $test1
 diff -r --exclude .commit-empty-directory reports/ test/reports/
-
 if [ $? -eq 0 ]
 then
-    echo 'PASS'
+    echo "PASS: \`${test1}\`"
 else
-    echo >&2 'FAIL'
+    echo >&2 "FAIL: \`${test1}\`"
 fi
