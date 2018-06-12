10da3a3f787efde6995a313279f976d732647c42
David <da70@nyu.edu>
2018-06-12 11:45:12 -0400

    bin/fetch-related-topics-json.sh: get related topics id file from command line arg

--- bin/fetch-related-topics-json.sh
+++ bin/fetch-related-topics-json.sh
@@ -2,11 +2,19 @@
 
 ROOT=$(cd "$(dirname "$0")" ; cd ..; pwd -P )
 
-topicIds=$( cat $ROOT/related-topics-ids-for-test-sample-tct-topics.txt )
+RELATED_TOPICS_IDS_FILE=$1
+
+if [ ! -e $RELATED_TOPICS_IDS_FILE ]
+then
+    echo >&2 "${RELATED_TOPICS_IDS_FILE} is not a valid related topics ids files."
+    exit 1
+fi
+
+topicIds=$( cat $RELATED_TOPICS_IDS_FILE )
 
 for topicId in $topicIds
 do
     curl https://nyuapi.infoloom.nyc/api/hit/basket/${topicId}/?format=json > $ROOT/test/tct/${topicId}.json
-    
+
     sleep 3
 done
