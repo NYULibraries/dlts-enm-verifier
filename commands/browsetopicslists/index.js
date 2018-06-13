eb338f4a8e75edc8bf6c065cc3573db185716a8c
David <da70@nyu.edu>
2018-06-13 12:11:35 -0400

    commands/browsetopicslists/index.js: initial commit of shell

---
+++ commands/browsetopicslists/index.js
@@ -0,0 +1,168 @@
+const fs        = require( 'fs' );
+const jsdom     = require( 'jsdom' );
+const { JSDOM } = jsdom;
+const _         = require( 'lodash' );
+const request   = require( 'sync-request' );
+
+const util      = require( '../../lib/util' );
+
+const commandName = 'browsetopicslists';
+
+const browseTopicsListCategories = [
+    '0-9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'enm-picks',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'non-alphanumeric',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+];
+
+var program,
+    directories,
+    topicsAllResponse,
+    enmCache, tctCache,
+    reportsDir;
+
+
+function init( programArg, directoriesArg ) {
+    program     = programArg;
+    directories = directoriesArg;
+
+    enmCache   = `${ directories.cache.enm }/${ commandName }`;
+    tctCache   = `${ directories.cache.tct }/${ commandName }`;
+    reportsDir = `${ directories.reports }/${ commandName }`;
+
+    util.clearDirectory( enmCache );
+    util.clearDirectory( tctCache );
+    util.clearDirectory( reportsDir );
+
+    program
+        .command( `${ commandName }` )
+        .action( verify );
+}
+
+function verify() {
+    topicsAllResponse = JSON.parse( getTopicsAllResponseBody() );
+
+    browseTopicsListCategories.forEach( browseTopicsListCategory => {
+        compareTctAndEnm( browseTopicsListCategory );
+    } );
+}
+
+function compareTctAndEnm( browseTopicsListCategory ) {
+    var tct = getTctData( browseTopicsListCategory ),
+        enm = getEnmData( browseTopicsListCategory ),
+
+        diffs = generateDiffs( tct, enm );
+
+    writeDiffReports( browseTopicsListCategory, diffs );
+}
+
+function getTctData( browseTopicsListCategory ) {
+    var tct = {};
+
+    tct.topics = getTctTopicsForBrowseTopicsListCategory( browseTopicsListCategory );
+
+    return tct;
+}
+
+function getEnmData( browseTopicsListCategory ) {
+    var enm = {};
+
+    enm.responseBody = getEnmResponseBody( browseTopicsListCategory );
+
+    enm.dom = new JSDOM( enm.responseBody );
+
+    enm.topics = getEnmTopicsFromBrowseTopicsList( enm.dom );
+    
+    return enm;
+}
+
+function getTopicsAllResponseBody() {
+    var responseBody;
+
+    if ( program.tctLocal ) {
+        responseBody = fs.readFileSync( `${ program.tctLocal }/TopicsAll.json`, 'utf8' );
+    } else {
+        responseBody = request( 'GET', `https://${ program.tctHost }/api/hit/basket/all/?format=json` ).getBody( 'utf8' );
+
+        // Cache response
+        fs.writeFileSync( `${ tctCache }/TopicsAll.json`, responseBody );
+    }
+
+    return responseBody;
+}
+
+function getEnmResponseBody( browseTopicsListCategory ) {
+    var responseBody;
+
+    if ( program.enmLocal ) {
+        responseBody = fs.readFileSync( `${ program.enmLocal }/${ browseTopicsListCategory }.html`, 'utf8' );
+    } else {
+        responseBody = request( 'GET', getEnmBrowseTopicsListUrl( browseTopicsListCategory ) ).getBody( 'utf8' );
+
+        fs.writeFileSync( `${ enmCache }/${ browseTopicsListCategory }.html`, responseBody );
+    }
+
+    return responseBody;
+}
+
+function getEnmBrowseTopicsListUrl( browseTopicsListCategory ) {
+    return `http://${ program.enmHost }/enm/enm-web/prototypes/browse-topics-lists/${ browseTopicsListCategory }.html`;
+}
+
+function getTctTopicsForBrowseTopicsListCategory( category ) {
+    // TODO
+    return [];
+}
+
+function getEnmTopicsFromBrowseTopicsList( dom ) {
+    // TODO
+    return [];
+}
+
+function generateDiffs( tct, enm ) {
+    var diffs = {};
+
+    diffs.topicsInTctNotInEnm = _.difference( tct.topics, enm.topics );
+    diffs.topicsInEnmNotTct = _.difference( enm.topics, tct.topics) ;
+
+    return diffs;
+}
+
+function writeDiffReports( browseTopicsListCategory, diffs ) {
+    if ( diffs.topicsInTctNotInEnm.length > 0 ) {
+        fs.writeFileSync( `${ reportsDir }/${ browseTopicsListCategory }-enm-missing-topics.json`,
+                          util.stableStringify( diffs.topicsInTctNotInEnm ) );
+    }
+
+    if ( diffs.topicsInEnmNotTct.length > 0 ) {
+        fs.writeFileSync( `${ reportsDir }/${ browseTopicsListCategory }-enm-extra-topics.json`,
+                          util.stableStringify( diffs.topicsInEnmNotTct ) );
+    }
+}
+
+module.exports.init = init;
