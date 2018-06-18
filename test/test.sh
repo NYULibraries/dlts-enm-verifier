f640f6dd025c70320fff97196e96360737b79d89
David <da70@nyu.edu>
2018-06-18 16:07:16 -0400

    test/: add test for solr command

--- test/test.sh
+++ test/test.sh
@@ -12,6 +12,9 @@ verify1='diff -r --exclude .commit-empty-directory reports/topicpages/ test/repo
 test2="node main.js browsetopicslists --enm-local=test/enm/browsetopicslists --tct-local=test/tct/browsetopicslists"
 verify2='diff -r --exclude .commit-empty-directory reports/browsetopicslists/ test/reports/browsetopicslists/'
 
+test3="node main.js solr --enm-local=test/enm/solr --tct-local=test/tct/solr -- 10-temp"
+verify3='diff -r --exclude .commit-empty-directory reports/solr/ test/reports/solr/'
+
 doTest() {
     local testCmd="$1"
     local verifyCmd="$2"
@@ -28,3 +31,4 @@ doTest() {
 
 doTest "${test1}" "${verify1}"
 doTest "${test2}" "${verify2}"
+doTest "${test3}" "${verify3}"
