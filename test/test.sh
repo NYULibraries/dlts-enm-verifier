7989c4984b2ff9f35f7d65650c0eac8133eb5b51
David <da70@nyu.edu>
2018-06-21 13:08:27 -0400

    test/test.sh: order tests by command name

--- test/test.sh
+++ test/test.sh
@@ -7,14 +7,14 @@ cd $ROOT
 locationIds=$( cat $ROOT/test/sample-location-ids.txt | tr '\n' ' ' )
 topicIds=$( cat $ROOT/test/sample-topics-ids.txt | tr '\n' ' ' )
 
-test[1]="node main.js topicpages --count-related-topics-occurrences --tct-local=test/tct/topicpages/ --enm-local=test/enm/topicpages/ -- ${topicIds}"
-verify[1]='diff -r --exclude .commit-empty-directory reports/topicpages/ test/reports/topicpages/'
+test[1]="node main.js browsetopicslists --enm-local=test/enm/browsetopicslists --tct-local=test/tct/browsetopicslists"
+verify[1]='diff -r --exclude .commit-empty-directory reports/browsetopicslists/ test/reports/browsetopicslists/'
 
-test[2]="node main.js browsetopicslists --enm-local=test/enm/browsetopicslists --tct-local=test/tct/browsetopicslists"
-verify[2]='diff -r --exclude .commit-empty-directory reports/browsetopicslists/ test/reports/browsetopicslists/'
+test[2]="node main.js solr --enm-local=test/enm/solr --tct-local=test/tct/solr -- ${locationIds}"
+verify[2]='diff -r --exclude .commit-empty-directory reports/solr/ test/reports/solr/'
 
-test[3]="node main.js solr --enm-local=test/enm/solr --tct-local=test/tct/solr -- ${locationIds}"
-verify[3]='diff -r --exclude .commit-empty-directory reports/solr/ test/reports/solr/'
+test[3]="node main.js topicpages --count-related-topics-occurrences --tct-local=test/tct/topicpages/ --enm-local=test/enm/topicpages/ -- ${topicIds}"
+verify[3]='diff -r --exclude .commit-empty-directory reports/topicpages/ test/reports/topicpages/'
 
 doTest() {
     local testCmd="$1"
